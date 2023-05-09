9faf32b5a5e6f3233075d55fa2bb7d319284a449
Carmen <uo264637@uniovi.es>
2023-05-09 12:19:35 +0200

    v1

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module project/go-vets-backend
 go 1.20
 
 require (
+	github.com/golang-jwt/jwt/v4 v4.5.0
 	github.com/gorilla/mux v1.8.0
 	github.com/rs/cors v1.9.0
 	go.mongodb.org/mongo-driver v1.11.6
--- handlers/handlers.go
+++ handlers/handlers.go
@@ -13,6 +13,9 @@ import (
 func Handlers() {
     router := mux.NewRouter()
     router.HandleFunc("/signUp", middlewares.CheckDB(routers.SignUp)).Methods("POST")
+    router.HandleFunc("/login", middlewares.CheckDB(routers.Login)).Methods("POST")
+    router.HandleFunc("/getUser", middlewares.CheckDB(middlewares.CheckJWT(routers.GetUser))).Methods("GET")
+    router.HandleFunc("/updateUser", middlewares.CheckDB(middlewares.CheckJWT(routers.UpdateUser))).Methods("PUT")
 
     PORT := os.Getenv("PORT")
     if PORT == "" {
